25855999ba442e9dc9393bb4af5f515b15912820
defilogist <defilogist@thewise.trade>
2023-11-15 00:13:37 +0100

    Handle Solana transactions to list cNFTs

--- src/tensor.js
+++ src/tensor.js
@@ -5,8 +5,10 @@ const {
   gql,
 } = require("@apollo/client")
 const { ApolloLink, concat } = require("apollo-link")
+const solanaWeb3 = require('@solana/web3.js');
 const fetch = require("cross-fetch")
 const uuid = require('uuid')
+const bs58 = require('bs58')
 
 const toSolami = (price) => {
   return price * 1_000_000_000
@@ -16,9 +18,49 @@ const fromSolami = (price) => {
   return price / 1_000_000_000
 }
 
+const getKeyPairFromB58SecretKey = (secretKey) => {
+  const privateKey = bs58.decode(secretKey)
+  const senderSecretKey = Uint8Array.from(privateKey)
+  return solanaWeb3.Keypair.fromSecretKey(senderSecretKey)
+}
+
+const runSolanaTransaction = async (
+  connection,
+  senderKeyPair,
+  transactionData
+) => {
+    console.log('Sending a transaction to the network')
+    const transactionBuffer = Buffer.from(transactionData)
+    const transaction = solanaWeb3.Transaction.from(transactionBuffer)
+    const { blockhash } = await connection.getRecentBlockhash()
+    transaction.recentBlockhash = blockhash
+    transaction.partialSign(senderKeyPair)
+    const signature = await connection.sendRawTransaction(
+      transaction.serialize(),
+      {
+        maxRetries: 5
+      }
+    )
+    await connection.confirmTransaction(signature)
+    console.log('Transaction sent:', signature)
+    return signature
+}
+
+
 class TensorClient {
-  constructor(apiKey) {
+  constructor(
+    apiKey,
+    privateKey = '',
+    network = 'devnet'
+  ) {
     this.initApolloClient(apiKey)
+    if (privateKey.length > 0) {
+      this.privateKey = getKeyPairFromB58SecretKey(privateKey)
+    }
+    this.network = network
+    this.connection = new solanaWeb3.Connection(
+      solanaWeb3.clusterApiUrl(network, 'confirmed')
+    )
   }
 
   initApolloClient(apiKey) {
@@ -76,7 +118,7 @@ class TensorClient {
     return fromSolami(data.statsV2.buyNowPrice)
   }
 
-  async listNFT(mint, walletAddress, price) {
+  async listcNFT(mint, walletAddress, price) {
     const query = gql`query TcompListTx(
       $mint: String!,
       $owner: String!,
@@ -95,8 +137,15 @@ class TensorClient {
       owner: walletAddress,
       price: '' + toSolami(price)
     }
-    const tx = await this.sendQuery(query, variables)
-    return tx.tcompListTx.txs
+    const data = await this.sendQuery(query, variables)
+    const transactionData = data.tcompListTx.txs[0].tx.data
+    await runSolanaTransaction(
+      this.connection,
+      this.privateKey,
+      transactionData
+    )
+    console.log(mint, 'listed')
+    return nft.mint
   }
 
   async runStopLossLoop(
@@ -115,10 +164,9 @@ class TensorClient {
       const floor = await this.getCollectionFloor(collectionSlug)
       if (floor <= limit) {
         nfts.forEach(async (nft) => {
-          const tx = await this.listNFT(
+          const tx = await this.listcNFT(
             nft.mint, nft.walletAddress, floor * percentage
           )
-          console.log(nft.mint, 'listed')
         })
       }
       return { floor, sold: floor * 0.9 }
